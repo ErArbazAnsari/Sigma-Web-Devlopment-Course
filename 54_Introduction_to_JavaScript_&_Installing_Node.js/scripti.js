ca75ca65e64555798b4c5e6754a2afd7cfb59358
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-02-15 19:16:52 +0530

    54_Introduction_to_JavaScript_

---
+++ 54_Introduction_to_JavaScript_&_Installing_Node.js/scripti.js
@@ -0,0 +1,22 @@
+console.log("This is Arbaz Ansari")
+console.log("This is Arbaz Ansari")
+console.log("This is Arbaz Ansari")
+
+console.warn("Your PC is Blasting")
+console.error("Shutdown your PC Please")
+// console.clear()
+
+document.title = "JavaScript Introduction"
+document.body.style.backgroundColor = "yellow";
+alert("Do You Want to Continue Further")
+
+var a = prompt("Enter You Name");
+console.log("Your Name is : ",a)
+
+document.write("Your Name is :",a)
+
+var a;
+a= prompt("Enter Your Number")
+if(a==0){
+    document.write("Your Number is ",a)
+}
