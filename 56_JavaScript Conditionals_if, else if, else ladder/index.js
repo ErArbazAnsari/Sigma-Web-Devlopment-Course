3cd60a8cb685c66fa0df2f53f68b11513e92dc53
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-02-15 23:55:20 +0530

    56_JavaScript If_Eles

---
+++ 56_JavaScript Conditionals_if, else if, else ladder/index.js
@@ -0,0 +1,31 @@
+console.log("My name is Arbaz Ansari")
+
+let age = 17;
+
+//If, Else
+if (age >= 18) {
+    console.log("You are eligible to drive.")
+}
+else {
+    console.log("You are not eligible to drive. Please Wait...")
+}
+
+//Operator in Javascript
+// +, -, *, /, **, %, ++, --
+let num1 = 2;
+let num2 = 3;
+
+console.log("+ ", num1 + num2);
+console.log("- ", num1 - num2);
+console.log("* ", num1 * num2);
+console.log("/ ", num1 / num2);
+console.log("** ", num1 ** num2);
+console.log("% ", num1 % num2);
+console.log("++ ", ++num1);
+console.log("-- ", ++num1);
+
+//Ternary Operator
+let ab = 5;
+let ba = 6;
+
+console.log(ab>ba?(ab-ba):(ba-ab));
