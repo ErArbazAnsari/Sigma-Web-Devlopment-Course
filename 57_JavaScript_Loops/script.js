93f1a568060fb0664ed91c79d842ab91674cf90b
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-02-16 16:19:51 +0530

    57_JavaScript_Loops

---
+++ 57_JavaScript_Loops/script.js
@@ -0,0 +1,20 @@
+// let sum = 0;
+// for (let i = 0; i <= 500000000; i++) {
+//     sum += i;
+//     console.log(sum)
+// }
+
+// let i = 0;
+// while (true) {
+//     i += 1;
+//     console.log("I'm Happy", i)
+// }
+
+// do {
+//     console.log("This is my name")
+//     i += 1;
+// } while (1);
+
+for (const iterator of "Arbaz Ansari") {
+    console.log(iterator)
+}
