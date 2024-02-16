0031d5c7855943e0131046b1bd78b41b8b1f632d
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-02-16 16:52:21 +0530

    JavaScript Fuctions

---
+++ 58_JavaScript_Functions/script.js
@@ -0,0 +1,27 @@
+// function myfuc(name) {
+//     console.log("Hi!,", name)
+// }
+
+// myfuc("Arbaz");
+
+
+// WAP to implement sum of two Number.
+// function sum(a, b) {
+//     console.log("Sum is:", a + b)
+// }
+
+// sum(10, 20);
+
+// function sum(a, b) {
+//     return (a+b);
+// }
+
+// let result = sum(22, 20);
+// console.log(result);
+
+//Arrow Function
+const myfuc = (x) => {
+    console.log("value of x is:", x);
+}
+
+myfuc(10);
