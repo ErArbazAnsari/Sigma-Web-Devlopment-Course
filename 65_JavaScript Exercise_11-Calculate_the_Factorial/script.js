d772657db76dd68c0ec5867d245b3e5b4c7f68e0
Arbaz Ansari <92993047+ErArbazAnsari@users.noreply.github.com>
2024-02-23 20:05:44 +0530

    new updated file 67
    Signed-off-by: Arbaz Ansari <92993047+ErArbazAnsari@users.noreply.github.com>

---
+++ 65_JavaScript Exercise_11-Calculate_the_Factorial/script.js
@@ -0,0 +1,34 @@
+// Write a program to calculate factorial of a number using reduce and using for loops
+// 6! = 6 * 5 * 4 * 3 * 2 * 1
+
+// function factorial(n){
+//     let fact = 1;
+//     if(n==0){
+//         return 0;
+//     }
+//     else{
+//         for(let i = 1; i<=n; i++){
+//             fact = fact*i;
+//         }
+//     }
+//     return fact;
+// }
+
+// let ans = factorial(18)
+// console.log(ans)
+
+
+// Reduce Function 
+var array = [];
+
+function factor(num) {
+    for (i = 1; i <= num; i++) {
+        array.push(i);
+    }
+    array.reduce(function (a, b) {
+        return a * b;
+    })
+};
+
+let ans = factor(5);
+console.log(ans)
