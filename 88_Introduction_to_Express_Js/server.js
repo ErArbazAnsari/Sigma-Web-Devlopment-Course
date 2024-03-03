b4f756be80de86d198f600d740bc3a427b0baa2d
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-03 10:52:09 +0530

    update work till 88_Introduction_to_Express_Js

--- 88_Introduction_to_Express_Js/server.js
+++ 88_Introduction_to_Express_Js/server.js
@@ -1,11 +1,23 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello Arbaz Ansari This is your home page')
+})
+
+app.get('/about/:slug/:second', (req, res) => {
+    // console.log(req)
+    console.log(req.params)
+    res.send(`hello ${req.params.slug} and ${req.params.second}`)
+    console.log(req.query)
+})
+
+// app.get('/contact', (req, res) => {
+//     res.send('Hello Arbaz Ansari This is your contact page')
+// })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
