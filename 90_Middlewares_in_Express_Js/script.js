43f98049591855dd244bd2a778413a064175cf1f
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-03 17:08:22 +0530

    89_Response,Request_and_Routers_in_Express

---
+++ 90_Middlewares_in_Express_Js/script.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}).get('/arbaz', (req, res) => {
+    res.send('Hello arbaz!')
+}).get('/home', (req, res) => {
+    res.send('Hello home!')
+}).get('/contact', (req, res) => {
+    res.send('Hello contact!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
