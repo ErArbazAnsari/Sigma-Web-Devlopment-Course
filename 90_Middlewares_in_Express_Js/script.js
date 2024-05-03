27b0d261ab239f0194b0bd7e738fd19423118a16
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-05-03 11:55:21 +0530

    some formatting changes

--- 90_Middlewares_in_Express_Js/script.js
+++ 90_Middlewares_in_Express_Js/script.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-}).get('/arbaz', (req, res) => {
-    res.send('Hello arbaz!')
-}).get('/home', (req, res) => {
-    res.send('Hello home!')
-}).get('/contact', (req, res) => {
-    res.send('Hello contact!')
-}).get('/happy', (req, res) => {
-    res.send('Hello contact!')
-}).get('/happy', (req, res) => {
-    res.send('Hello contact!')
-})
+app
+  .get("/", (req, res) => {
+    res.send("Hello World!");
+  })
+  .get("/arbaz", (req, res) => {
+    res.send("Hello arbaz!");
+  })
+  .get("/home", (req, res) => {
+    res.send("Hello home!");
+  })
+  .get("/contact", (req, res) => {
+    res.send("Hello contact!");
+  })
+  .get("/happy", (req, res) => {
+    res.send("Hello contact!");
+  })
+  .get("/happy", (req, res) => {
+    res.send("Hello contact!");
+  });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
